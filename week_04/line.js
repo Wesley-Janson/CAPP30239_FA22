306da33de0c5ce4a812d24494f925a5cea7afde6
Wesley Janson <wrjanson@Wesleys-MacBook-Air.local>
2022-10-20 15:26:04 -0500

    Work from today's class

--- week_04/line.js
+++ week_04/line.js
@@ -1,5 +1,6 @@
 /* D3 Line Chart */
 
+// Variables are on top because they are not dependant on data
 const height = 500,
     width = 800,
     margin = ({ top: 15, right: 30, bottom: 35, left: 40 });
@@ -9,32 +10,56 @@ const svg = d3.select("#chart")
     .attr("viewBox", [0, 0, width, height]);
 
 d3.csv('long-term-interest-monthly.csv').then(data => {
-    
-    //TYPE HERE
-    
-    // svg.append("g")
-    //   .attr("transform", `translate(0,${height - margin.bottom})`)
-    //   .call(d3.axisBottom(x));
-    
-    // svg.append("g")
-    //   .attr("transform", `translate(${margin.left},0)`)
-    //   .call(d3.axisLeft(y));
+    let timeParse = d3.timeParse("%Y-%m");
 
-    // svg.append("text")
-    //   .attr("class", "x-label")
-    //   .attr("text-anchor", "end")
-    //   .attr("x", width - margin.right)
-    //   .attr("y", height)
-    //   .attr("dx", "0.5em")
-    //   .attr("dy", "-0.5em") 
-    //   .text("Year");
+    for (let d of data) {
+        d.Value = +d.Value;
+        d.Date = timeParse(d.Date);
+    }
+
+    let x = d3.scaleTime()
+        .domain(d3.extent(data, d => d.Date))
+        .range([margin.left, width - margin.right]);
+
+    let y = d3.scaleLinear()
+        .domain([0,d3.max(data, d => d.Value)])
+        .range([height - margin.bottom, margin.top]);
+
+    svg.append("g")
+      .attr("transform", `translate(${margin.left},0)`)
+      .attr("class", "y-axis")
+      .call(d3.axisLeft(y).tickSizeOuter(0).tickFormat(d => d + "%").tickSize(-width));
     
-    // svg.append("text")
-    //   .attr("class", "y-label")
-    //   .attr("text-anchor", "end")
-    //   .attr("x", -margin.top/2)
-    //   .attr("dx", "-0.5em")
-    //   .attr("y", 10)
-    //   .attr("transform", "rotate(-90)")
-    //   .text("Interest rate");
+    svg.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    svg.append("text")
+      .attr("class", "x-label")
+      .attr("text-anchor", "end")
+      .attr("x", width - margin.right)
+      .attr("y", height)
+      .attr("dx", "0.5em")
+      .attr("dy", "-0.5em") 
+      .text("Year");
+    
+    svg.append("text")
+      .attr("class", "y-label")
+      .attr("text-anchor", "end")
+      .attr("x", -margin.top/2)
+      .attr("dx", "-0.5em")
+      .attr("y", 10)
+      .attr("transform", "rotate(-90)")
+      .text("Interest rate");
+
+    let line = d3.line()
+        .x(d => x(d.Date))
+        .y(d => y(d.Value));
+
+    svg.append("path")
+        .datum(data)
+        .attr("d", line)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue");
+
   });
