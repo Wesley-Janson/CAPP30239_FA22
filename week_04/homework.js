1ba969900d89668710ca6e0813d19fcb91b8ed3f
Wesley Janson <wrjanson@Wesleys-MacBook-Air.local>
2022-10-23 18:45:15 -0500

    Comment code

--- week_04/homework.js
+++ week_04/homework.js
@@ -9,31 +9,38 @@ const svg = d3.select("#chart")
     .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
+// Load in data, create timeParse function
 d3.csv('long-term-interest-canada.csv').then(data => {
     let timeParse = d3.timeParse("%Y-%m");
 
+    // Coerce "Num" to a numeric, "Month" to date
     for (let d of data) {
         d.Num = +d.Num;
         d.Month = timeParse(d.Month);
     }
 
+    // Create x-axis 
     let x = d3.scaleTime()
         .domain(d3.extent(data, d => d.Month))
         .range([margin.left, width - margin.right]);
 
+    // Create y-axis
     let y = d3.scaleLinear()
         .domain([0,d3.max(data, d => d.Num)])
         .range([height - margin.bottom, margin.top]);
 
+    // Create y-axis tick marks, specialize them
     svg.append("g")
       .attr("transform", `translate(${margin.left},0)`)
       .attr("class", "y-axis")
       .call(d3.axisLeft(y).tickSizeOuter(0).tickFormat(d => d + "%").tickSize(-width));
     
+    // Create x-axis tick marks 
     svg.append("g")
       .attr("transform", `translate(0,${height - margin.bottom})`)
       .call(d3.axisBottom(x).tickSizeOuter(0));
 
+    // Create x-axis label
     svg.append("text")
       .attr("class", "x-label")
       .attr("text-anchor", "end")
@@ -43,6 +50,7 @@ d3.csv('long-term-interest-canada.csv').then(data => {
       .attr("dy", "-0.5em") 
       .text("Month");
     
+    // Create y-axis label
     svg.append("text")
       .attr("class", "y-label")
       .attr("text-anchor", "end")
@@ -52,10 +60,12 @@ d3.csv('long-term-interest-canada.csv').then(data => {
       .attr("transform", "rotate(-90)")
       .text("Interest Rate");
 
+    // Create line to be placed in chart
     let line = d3.line()
         .x(d => x(d.Month))
         .y(d => y(d.Num));
 
+    // Add line to graph
     svg.append("path")
         .datum(data)
         .attr("d", line)
