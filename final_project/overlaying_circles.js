12885678e5a6bc016cba68d3891093576005884c
Wesley Janson <wrjanson@Wesleys-MacBook-Air.local>
2022-11-14 18:12:28 -0600

    Added Week 8 folder

--- final_project/overlaying_circles.js
+++ final_project/overlaying_circles.js
@@ -3,12 +3,8 @@ CAPP 30239 Final Project
 Figure 2 - Overlaying Circles Graph
 */
 
-
-
-
-d3.csv("overlap_test.csv").then(data => {
+d3.csv("overlap_circles.csv").then(data => {
     for (let d of data) {
-        console.log(d)
         createCircle(d);   // Loop through CSV, run function to create circles for each data point (Component)
     }
 });
@@ -27,52 +23,40 @@ function createCircle({ Base, Max, Current, Component }) {
         .attr("viewBox", [0,0,width,height]);
 
     circle_svg
-        .selectAll("circle")
-        //.data(data)
-        .join("circle")
+        .append("circle")
         .attr("cx", width/2)
         .attr("cy", height/2)
         .attr("r", d => {
-            console.log(base_circle*(1 + Max))
             return base_circle*(1 + Max)
+        })
+        .style("fill", "blue")
+        .attr("opacity", 0.15);
+
+    circle_svg
+        .append("circle")
+        .attr("cx", width/2)
+        .attr("cy", height/2)
+        .attr("r", d => {
+            return base_circle*(1 + Current)
         })
         .style("fill", "blue")
         .attr("opacity", 0.15)
 
     circle_svg
         .append("circle")
-        //.data(data)
-        .join("circle")
         .attr("cx", width/2)
         .attr("cy", height/2)
         .attr("r", d => {
-            console.log(base_circle*(1 + Current))
-            return base_circle*(1 + Current)
-        })
-        .style("fill", "blue")
-        .attr("opacity", 0.4)
-
-    circle_svg
-        .append("circle")
-        //.data(data)
-        .join("circle")
-        .attr("cx", width/2)
-        .attr("cy", height/2)
-        .attr("r", d => {
-            console.log(base_circle)
             return base_circle
         })
         .style("fill", "blue")
-        .attr("opacity", 0.65)
+        .attr("opacity", 0.15)
 
     circle_svg
         .append("text")
         .attr("font-size", 1)
-        .attr("font-weight", "bold")
-        .attr("text-anchor", "middle")
         .attr("alignment-baseline", "middle")
         .text(Component)
         .style("font-size", 1);
 
 }
-
