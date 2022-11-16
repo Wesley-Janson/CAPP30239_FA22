fc618324c738f639caa2c4da2a4c1779fde85d40
Wesley Janson <wrjanson@Wesleys-MacBook-Air.local>
2022-11-15 18:32:42 -0600

    Changes from lecture

--- week_08/chord/chord.js
+++ week_08/chord/chord.js
@@ -22,10 +22,10 @@ d3.csv("../degrees.csv").then((data) => {
 
   // This code returns ["Males", "Females", "Associate", "Bachelors", "Masters", "Doctors"];
   let arr1 = d3.map(data, d => d.source),
-    arr2 = d3.map(data, d => d.target);
+    arr2 = d3.map(data, d => d.target);  //Take each value and push into new array
   let names = arr1.concat(arr2);
 
-  names = [...new Set(names)]; //spread syntax
+  names = [...new Set(names)]; //spread syntax-this expands everything in array, keeping only unique values
 
   // This is an alternative way to do the same thing
   // let names = [
@@ -60,49 +60,49 @@ d3.csv("../degrees.csv").then((data) => {
 
   const color = d3.scaleOrdinal(names, [d3.schemeTableau10[4], d3.schemeTableau10[6], "#333", "#333", "#333", "#333"]);
 
-  // svg.append("path")
-  //   .attr("id", "text-id")
-  //   .attr("fill", "none")
-  //   .attr("d", d3.arc()({ outerRadius, startAngle: 0, endAngle: 2 * Math.PI }));
+  svg.append("path")
+    .attr("id", "text-id")
+    .attr("fill", "none")
+    .attr("d", d3.arc()({ outerRadius, startAngle: 0, endAngle: 2 * Math.PI }));
 
-  // svg.append("g")
-  //   .selectAll("g")
-  //   .data(chords)
-  //   .join("path")
-  //   .attr("d", ribbon)
-  //   .attr("fill", d => color(names[d.source.index]))
-  //   .attr("fill-opacity", 0.75)
-  //   .style("mix-blend-mode", "multiply")
-  //   .on("mouseover", function() {
-  //     d3.select(this)
-  //       .attr("fill-opacity", 1);
-  //   })
-  //   .on("mouseout", function() {
-  //     d3.select(this)
-  //       .attr("fill-opacity", 0.75);
-  //   })
-  //   .on("click", function(e, d) {
-  //     let str = `${names[d.source.index]} earned 
-  //       ${d.source.value.toLocaleString()} ${names[d.target.index]} Degrees`;
-  //     d3.select("h2")
-  //       .html(str);
-  //   })
-  //   .append("title")
-  //   .text(d => `${names[d.source.index]} earned ${d.source.value.toLocaleString()} ${names[d.target.index]} Degrees`);
+  svg.append("g")
+    .selectAll("g")
+    .data(chords)
+    .join("path")
+    .attr("d", ribbon)
+    .attr("fill", d => color(names[d.source.index]))
+    .attr("fill-opacity", 0.75)
+    .style("mix-blend-mode", "multiply")
+    .on("mouseover", function() {
+      d3.select(this)
+        .attr("fill-opacity", 1);
+    })
+    .on("mouseout", function() {
+      d3.select(this)
+        .attr("fill-opacity", 0.75);
+    })
+    .on("click", function(e, d) {
+      let str = `${names[d.source.index]} earned 
+        ${d.source.value.toLocaleString()} ${names[d.target.index]} Degrees`;
+      d3.select("h2")
+        .html(str);
+    })
+    .append("title")
+    .text(d => `${names[d.source.index]} earned ${d.source.value.toLocaleString()} ${names[d.target.index]} Degrees`);
 
-  // svg.append("g")
-  //   .selectAll("g")
-  //   .data(chords.groups)
-  //   .join("g")
-  //   .call(g => g.append("path")
-  //     .attr("d", arc)
-  //     .attr("fill", d => color(names[d.index]))
-  //     .attr("stroke", "#fff"))
-  //   .call(g => g.append("text")
-  //     .attr("dy", -3)
-  //     .append("textPath")
-  //     .attr("href", "#text-id")
-  //     .attr("startOffset", d => d.startAngle * outerRadius)
-  //     .text(d => names[d.index]))
-  //     .attr("fill", d => color(names[d.index]) != "#ccc" ? color(names[d.index]) : "#666");
+  svg.append("g")
+    .selectAll("g")
+    .data(chords.groups)
+    .join("g")
+    .call(g => g.append("path")
+      .attr("d", arc)
+      .attr("fill", d => color(names[d.index]))
+      .attr("stroke", "#fff"))
+    .call(g => g.append("text")
+      .attr("dy", -3)
+      .append("textPath")
+      .attr("href", "#text-id")
+      .attr("startOffset", d => d.startAngle * outerRadius)
+      .text(d => names[d.index]))
+      .attr("fill", d => color(names[d.index]) != "#ccc" ? color(names[d.index]) : "#666");
 });
